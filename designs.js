e3df6ec0da81732b5daa5313769daaea00c60040
amyruth <anrutherford@gmail.com>
2018-03-28 17:28:38 -0400

    removed some spaces

--- designs.js
+++ designs.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-// v2 has right click to erase option
-=======
 /* eslint-env jquery */
-
->>>>>>> 6bbf004b384f098c60aa985e4b872eff7c1b0b48
 
 function makeGrid(row, col) {
 	//clears screen if grid exists
@@ -18,45 +13,6 @@ function makeGrid(row, col) {
 	};
 }
 
-<<<<<<< HEAD
-$(document).ready(function(){
-    const clearGrid = $('#clearGrid');
-    const pixelCanvas = $('#pixelCanvas');
-    let color;
-
-    $('#sendVals').on('click', function(evt){
-        //get new values and create grid
-        evt.preventDefault();
-        let rows, columns;
-        rows = $('#inputHeight').val();
-        columns = $('#inputWidth').val();
-
-        makeGrid(rows, columns);
-    });
-
-    pixelCanvas.on('click', 'td', function(color){
-        //grabs color value and applies it to specific td element
-        color = $('#colorPicker').val();
-        $(this).css('background-color', color);
-        $(this).addClass('filled');
-        console.log($(this).css('background-color'));
-        //works when eraser button is clicked
-        if(eraserActive){
-            $(this).css('background-color', 'transparent');
-        }
-    });
-
-    clearGrid.click(function(){
-        //reverts entire grid to white
-       $('td').css('background-color', 'transparent');
-    });
-
-   //right-click to erase care of @DN_1524. Thank you! 
-   pixelCanvas.on('contextmenu', 'td', function (e){
-        e.preventDefault();
-        $(this).css('background-color', 'transparent');
-    });
-=======
 $(document).ready(function () {
 	const clearGrid = $('#clearGrid');
 	const eraseCell = $('#eraseCell');
@@ -102,6 +58,5 @@ $(document).ready(function () {
 			console.log(eraserActive);
 		};
 	});
->>>>>>> 6bbf004b384f098c60aa985e4b872eff7c1b0b48
 
 });
